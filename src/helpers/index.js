6b15c1a7b4286f2183150fb76d6ef221193c78a1
dhiegoF <dhiegogithub@gmail.com>
2022-08-14 19:50:25 +0000

    web

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,35 +1,38 @@
-const fs = require('fs-extra')
+const fs = require("fs-extra");
 
-const __main__ = "__main__"
+const __main__ = "__main__";
 
 async function main() {
-  while(true) {
-    try {
-      const workingFile = fs.readFileSync('./dist_incomplete/code.json').toString().split('\n')
-      const completeWork = JSON.stringify(fs.readJSONSync('./dist/code.json')).split(',')
+  try {
+    const workingFile = fs
+      .readFileSync("./dist_incomplete/code.json")
+      .toString()
+      .split("\n");
+    const completeWork = JSON.stringify(
+      fs.readJSONSync("./dist/code.json")
+    ).split(",");
 
-      let finded = false
+    let finded = false;
 
-      const lastRow = workingFile.reverse()[0]
-      const lastAlpha = lastRow.match(/"[A-Z][A-Z]"/g)
+    const lastRow = workingFile.reverse()[0];
+    const lastAlpha = lastRow.match(/"[A-Z][A-Z]"/g);
 
-      for(let line of completeWork) {
-        if(finded) {
-          console.log(line)
+    for (let line of completeWork) {
+      if (finded) {
+        console.log(line);
 
-          finded = false
-        }
-
-        if(line.indexOf(lastAlpha) != -1) {
-          finded = true
-        }
+        finded = false;
       }
-    }catch(_) {
-      // pass
+
+      if (line.indexOf(lastAlpha) != -1) {
+        finded = true;
+      }
     }
+  } catch (_) {
+    // pass
   }
 }
 
-if(__main__ == "__main__") {
-  main()
+if (__main__ == "__main__") {
+  main();
 }
